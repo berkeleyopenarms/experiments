0441769a9898e99450d7ccaae5d835a0740288cb
Brent Yi <yibrenth@gmail.com>
2017-02-14 13:38:22 -0800

    pretty much done w/ load cell ros node

--- loadCellReaderROS/loadCellReaderROS.ino
+++ loadCellReaderROS/loadCellReaderROS.ino
@@ -1,38 +1,71 @@
 #include <ros.h>
 #include <std_msgs/Float32.h>
+#include <std_srvs/Trigger.h>
+#include <AccelStepper.h>
 
 ros::NodeHandle nh;
+
 
 std_msgs::Float32 force_msg;
 ros::Publisher force_pub("strain_gauge", &force_msg);
 
+//TODO: test service calls more thoroughly? it doesn't seem to be a thoroughly tested feature of rosserial
+//ros::ServiceClient<std_srvs::Trigger::Request, std_srvs::Trigger::Response> zero_client("zero_motor");
+
+#define STEPPER_SPEED 2000
+#define STEPS_PER_MM 400 // 200 steps per revolution * 16 microsteps / 8mm per revolution (TODO: fix this)
+#define READ_SAMPLES 500
+#define STRAIN_GAUGE_IN A0
+#define LIMIT_SWITCH_IN 1
+#define STEPPER_STEP_OUT 2
+#define STEPPER_DIR_OUT 3
+
+AccelStepper stepper(AccelStepper::DRIVER, STEPPER_STEP_OUT, STEPPER_DIR_OUT);
+
+void stepperCb( const std_msgs::Float32& position_msg) {
+  stepper.moveTo((long) (position_msg.data * STEPS_PER_MM));
+}
+
+void zeroCb (const std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
+  stepper.setSpeed(-1 * STEPPER_SPEED);
+  while (digitalRead(LIMIT_SWITCH_IN)) {
+    stepper.runSpeed();
+  }
+}
+
+ros::Subscriber<std_msgs::Float32> sub("stepper_position", &stepperCb );
+ros::ServiceServer<std_srvs::Trigger::Request, std_srvs::Trigger::Response> zero_server("zero_motor", &zeroCb);
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(STRAIN_GAUGE_IN, INPUT);
+  pinMode(LIMIT_SWITCH_IN, INPUT);
+  analogReadResolution(16);
+
+  stepper.setCurrentPosition(0);
+
   nh.initNode();
   nh.advertise(force_pub);
-  
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  analogReadResolution(16);
+  nh.advertiseService(zero_server);
+  while (!nh.connected()) nh.spinOnce();
+  nh.loginfo("Started up");
 }
-float val1;
-float val2;
+
 float avg = 0 ;
 float cons = 21128;
-float mult = 100.0/60.0;
+float mult = 100.0 / 60.0;
 float saved_val = 0;
-#define n 500.0
 
 void loop() {
-  val1 =0;
-  
-  for (int i = 0; i<n; i++){
-    val1 = val1 + analogRead(A0);
+  float sum = 0;
+  for (int i = 0; i < READ_SAMPLES; i++) {
+    sum += analogRead(STRAIN_GAUGE_IN);
   }
-  val1 = val1/n;
-  avg = avg*0.93 +  val1*0.07;
-  
-  force_msg.data = avg;
+  avg = avg * 0.93 +  sum * 0.07 / READ_SAMPLES;
+
+  force_msg.data = (avg - cons) * mult;
   force_pub.publish( &force_msg );
+
+  stepper.runSpeed();
+
   nh.spinOnce();
 }
