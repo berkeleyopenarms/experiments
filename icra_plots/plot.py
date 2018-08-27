4a735506fdc749fa02a2152e06f2cc5f56778191
Brent Yi <yibrenth@gmail.com>
2018-08-26 23:28:50 -0700

    more data

--- icra_plots/plot.py
+++ icra_plots/plot.py
@@ -9,15 +9,15 @@ def load_from_csv(path):
     return labels, data
 
 
-# time,x,y,z,qx,qy,qz,qw,j0,j1,j2,j3,j4,j5,j6
-_, ee_data = load_from_csv("ee.csv")
-
-# time,x,y,z,qx,qy,qz,qw
-# _, ee_data = load_from_csv("vive.csv")
-
-ee_data_xyz = ee_data[::20,1:4].T
-print(ee_data_xyz.shape)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(ee_data_xyz[0], ee_data_xyz[1], ee_data_xyz[2], linewidth=1)
+
+_, data = load_from_csv("data/cmd.csv")
+data_xyz = data[::40,1:4].T
+ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
+
+_, data = load_from_csv("data/ee.csv")
+data_xyz = data[::40,1:4].T
+ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
+
 plt.show()
