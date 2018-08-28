ce008490b00863f355c4017d7cfacb56aff92ae3
Philipp <phil80301@berkeley.edu>
2018-08-27 21:55:29 -0700

    intervals for end effoctor

--- icra_plots/plot.py
+++ icra_plots/plot.py
@@ -8,16 +8,69 @@ def load_from_csv(path):
     data = raw[1:].astype(np.float)
     return labels, data
 
+def get_intervals(cmd, cmd_data, rel_data):
+    intervals = []
+    joints = cmd_data[:,-7:]
+    for i, j in enumerate(joints):
+        if np.sum((cmd - j)**2) < 1.0e-4 and 1.0 < cmd_data[i+1,0] - cmd_data[i,0]:
+            intervals.append([cmd_data[i,0], cmd_data[i+1,0]])
+            # print(cmd_data[i,0]-cmd_data[i+1,0])
+            print(cmd_data[i,0], cmd_data[i+1,0])
+    intervals = np.array(intervals)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+    data = []
+    for i in intervals:
+        vive_int = []
+        for d in rel_data:
+            if d[0] >= i[0] + 2.5 and d[0] <= i[1]:
+                vive_int.append(d)
+        data.append(np.array(vive_int))
+    return data;
 
-_, data = load_from_csv("data/cmd.csv")
-data_xyz = data[::40,1:4].T
-ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
+def plot_quigley():
+    _, data = load_from_csv("q_data/cmd.csv")
+    _, vive = load_from_csv("q_data/vive.csv")
+    _, start_end = load_from_csv("q_data/home.csv")
+    start_vive = get_intervals(start_end[0], data, vive)
 
-_, data = load_from_csv("data/ee.csv")
-data_xyz = data[::40,1:4].T
-ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for x in start_vive:
+        data_xyz = x[::40,1:4].T
+        ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
 
-plt.show()
+def plot_vive():
+    _, data = load_from_csv("pp_data/cmd.csv")
+    _, vive = load_from_csv("pp_data/vive.csv")
+    _, start_end = load_from_csv("pp_data/start_and_end_joints.csv")
+
+    start_vive = get_intervals(start_end[0], data, vive)
+    end_vive = get_intervals(start_end[1,:], data, vive)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for x in start_vive:
+        data_xyz = x[::40,1:4].T
+        ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
+    for x in end_vive:
+        data_xyz = x[::40,1:4].T
+        ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
+    plt.show()
+
+def plot_traj():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    _, data = load_from_csv("pp_data/cmd.csv")
+    data_xyz = data[::40,1:4].T
+    ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
+
+    _, data = load_from_csv("pp_data/ee.csv")
+    data_xyz = data[::40,1:4].T
+    ax.plot(data_xyz[0], data_xyz[1], data_xyz[2], linewidth=1)
+
+    plt.show()
+
+# plot_vive()
+# plot_quigley()
+# plot_traj()
