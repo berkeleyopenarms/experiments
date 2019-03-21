ccd900fa25e7b39147f1bdeea9ae90d418b76806
Brent Yi <brentyi@berkeley.edu>
2019-03-21 02:56:58 -0500

    add start stop script

---
+++ start_stop/start_stop.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# A basic example of using BlueInterface for joint positions control.
+# It allows a user to record four sets of joint positions by manually moving the arm to each
+# position and pressing enter. It then plays back a trajectory comprised of the four sets of
+# joint positions in an infinite loop.
+
+import sys
+from blue_interface import BlueInterface
+import numpy as np
+import time
+
+if __name__ == '__main__':
+
+    blue_right = BlueInterface("right", "127.0.0.1")
+    blue_left = BlueInterface("left", "127.0.0.1")
+
+
+    while True:
+        blue_right.disable_control()
+        blue_left.disable_control()
+        input("Press [Enter] to hold position...")
+        blue_right.set_joint_positions(blue_right.get_joint_positions())
+        blue_left.set_joint_positions(blue_left.get_joint_positions())
+        input("Press [Enter] to for gravity compensation mode...")
+
+    # blue.shutdown()
